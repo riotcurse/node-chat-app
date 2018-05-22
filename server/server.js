27d11018650729c0345d3a424754a6e0868ce1b1
riotkurz <rio.h.kurz@gmail.com>
2018-05-22 07:26:08 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(publicPath));
+
+// app.get('/',(req,res)=>{
+//   res.sendFile(publicPath + '/index.html');
+// });
+
+app.listen(3000,()=>{
+  console.log(`Started on port ${port}`);
+});
+
+module.exports = {app};
